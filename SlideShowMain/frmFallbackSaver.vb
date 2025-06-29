602eebf735dadf559ac498ff7d271e2fd6b03b18
Arne Schwarzer <arne@schwarzer-hh.de>
2025-06-29 17:38:03 +0200

    Kommentierung Fallbacksaver

--- SlideShowMain/frmFallbackSaver.vb
+++ SlideShowMain/frmFallbackSaver.vb
@@ -10,17 +10,23 @@ Public Class frmFallbackSaver
     Inherits Form
 
     '-- Variablen Deklarationen --
-    Private bewegungsWinkel As Double = 45
-    Private bewegungsgeschwindigkeit As Double = 4.0
-    Private ReadOnly randPuffer As Integer = 5
-    Private rnd As New Random()
-    Private picSplashscreen As PictureBox
+    Private Shared bewegungsWinkel As Double = 45
+    Private Shared bewegungsgeschwindigkeit As Double = 4.0
+    Private Shared ReadOnly randPuffer As Integer = 5
+    Private Shared rnd As New Random()
+    Private Shared picSplashscreen As PictureBox
 
     Private Sub frmFallbacksaver_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'Fallbacksaver Form initialisieren
+
         FormsHandling.InitialFormPreparation(Me, Color.Black)
+
     End Sub
 
     Private Sub frmFallbacksaver_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        'Fallbacksaver Form inital anzeigen
+
+        'Das Logo in der Mitte des Bildschirms platzieren
         picSplashscreen = New PictureBox With {
             .Image = DirectCast(Splashscreen, Image),
             .SizeMode = PictureBoxSizeMode.AutoSize,
@@ -31,11 +37,15 @@ Public Class frmFallbackSaver
         Me.Controls.Add(picSplashscreen)
         picSplashscreen.Visible = True
 
+        'Timer starten
         tmrFallback.Interval = 30
         tmrFallback.Start()
+
     End Sub
 
     Private Sub tmrFallback_Tick(sender As Object, e As EventArgs) Handles tmrFallback.Tick
+        'Der eigentliche Schoner - Das Logo wird bewegt
+
         Dim pos As Point = picSplashscreen.Location
 
         Dim dx As Double = Math.Cos(bewegungsWinkel * Math.PI / 180) * bewegungsgeschwindigkeit
@@ -66,9 +76,13 @@ Public Class frmFallbackSaver
             Math.Max(0, Math.Min(clientWidth - picSplashscreen.Width, CInt(picSplashscreen.Left + dx))),
             Math.Max(0, Math.Min(clientHeight - picSplashscreen.Height, CInt(picSplashscreen.Top + dy)))
         )
+
     End Sub
 
     Private Function KorrigiereWinkel(winkel As Double) As Double
+        'Hilfsfunktion die verhindert, dass das Logo sich in "langweiligen" horizontalen
+        'oder vertikalen Geraden auf dem Bildschirm bewegt
+
         Dim erlaubteBasiswinkel As Double() = {45, 135, 225, 315}
         Dim minAbweichung As Double = 0
         Dim maxAbweichung As Double = 20
@@ -84,6 +98,8 @@ Public Class frmFallbackSaver
     End Function
 
     Private Function NormalisiereWinkel(w As Double) As Double
+        'Korrektur des eingegebenen Winkels auf den 0° - 360° Normbereich
+
         While w < 0
             w += 360
         End While
@@ -91,13 +107,18 @@ Public Class frmFallbackSaver
             w -= 360
         End While
         Return w
+
     End Function
 
     Private Sub frmFallbackSaver_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
+        'Weiterleitung von Maus-Buttons an das MCP
+
         KeyAndMouseHandling.ForwardMouseDown(Me, e)
     End Sub
 
     Private Sub frmFallbackSaver_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        'Weiterleitung von Tastatureingaben an das MCP
+
         KeyAndMouseHandling.ForwardKeyDown(Me, e)
     End Sub
 
